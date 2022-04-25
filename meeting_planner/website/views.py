02a33f6bed1e9cf0e161264202c09432c1f74341
yessermiranda13 <yesser.miranda@uraccan.edu.ni>
2022-04-25 11:47:01 -0600

    Agregando nuevas vistas al proyecto, para pruebas de url

--- meeting_planner/website/views.py
+++ meeting_planner/website/views.py
@@ -1,5 +1,13 @@
+from datetime import datetime
+
 from django.http import HttpResponse
 from django.shortcuts import render
 
 def welcome(request):
     return HttpResponse('Bienvenido al planificador de reuniones')
+
+def date(request):
+    return HttpResponse('Esta pagina fue mostrada el ' + str(datetime.now()))
+
+def about(request):
+    return HttpResponse('Mi nombre es Yesser Miranda')
